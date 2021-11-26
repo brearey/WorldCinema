10ecb21bae286caffd3f7401c061d9fe597c4cac
Brearey <brearey4@gmail.com>
2021-11-26 18:55:54 +0900

    Initial commit

---
+++ app/src/main/java/ru/oktemsec/worldcinema/SigninActivity.kt
@@ -0,0 +1,20 @@
+package ru.oktemsec.worldcinema
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+
+        val btnRegister: Button = findViewById(R.id.btnRegister)
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
